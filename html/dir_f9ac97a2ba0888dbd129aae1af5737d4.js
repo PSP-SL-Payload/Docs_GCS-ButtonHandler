e8ea5866b98087338b710620bdc2565026f0227c
Kade Boltjes <54650222+Kboltjes@users.noreply.github.com>
2022-06-18 17:48:26 -0400

    Delete html directory

--- html/dir_f9ac97a2ba0888dbd129aae1af5737d4.js
+++
@@ -1,5 +0,0 @@
-var dir_f9ac97a2ba0888dbd129aae1af5737d4 =
-[
-    [ "pinouts.h", "d7/db5/pinouts_8h.html", "d7/db5/pinouts_8h" ],
-    [ "Switches.h", "dc/d66/_switches_8h.html", "dc/d66/_switches_8h" ]
-];
